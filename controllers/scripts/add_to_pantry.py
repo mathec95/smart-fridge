900f103b995c2058501a0c0158bc292b8ff4f81e
mathec95 <mathesonec@gmail.com>
2018-05-26 16:47:51 +0000

    26 May 2018

--- controllers/scripts/add_to_pantry.py
+++ controllers/scripts/add_to_pantry.py
@@ -1,48 +1,48 @@
 #! /usr/bin/env python
 
+import sys
 import MySQLdb
+import argparse
 
-# continue to loop through code until "quit" is entered
-while True:
-	# query user for a barcode
-	barcode = raw_input("Please enter barcode to add to pantry:")
+parser = argparse.ArgumentParseR()
+parser.add_argument("barcode"
+args = parser.parse_args()
+barcode = args.barcode
 
-	if barcode == "quit":
-		break
-	# open database connection
-	db = MySQLdb.connect("localhost","emma","sudosudo!!","pantry")
+# open database connection
+db = MySQLdb.connect("localhost","emma","sudosudo!!","pantry")
 
-	cursor = db.cursor()
+cursor = db.cursor()
 
-	# check purchases list for matching barcode
-	cursor.execute("SELECT barcode_num FROM purchases WHERE barcode_num = '%s'" % barcode)
-	# if this barcode is not found in purchases
+# check purchases list for matching barcode
+cursor.execute("SELECT barcode_num FROM purchases WHERE barcode_num = '%s'" % barcode)
+
+# if this barcode is not found in purchases
+if cursor.rowcount == 0:
+	# check database for a matching barcode
+	cursor.execute("SELECT barcode_num FROM product_list WHERE barcode_num = '%s'" % barcode)
+	# if this barcode is not in database:
+#	figure out how to render a form from this file in html.
 	if cursor.rowcount == 0:
-		# check database for a matching barcode
-		cursor.execute("SELECT barcode_num FROM product_list WHERE barcode_num = '%s'" % barcode)
-		# if this barcode is not in database:
-		if cursor.rowcount == 0:
-			name = raw_input("Name: ")
-			category = raw_input("Category: ")
-			# add item to database
-			cursor.execute("INSERT INTO product_list(name, barcode_num, category) VALUES ('%s', '%s', '%s')" % (name, barcode, category))
-			if cursor.rowcount == 1:
-				print "item added to database successfully"
-			db.commit()
-		# add item to purchases list
-		cursor.execute("INSERT INTO purchases(barcode_num) VALUES ('%s')" % barcode)
-		if cursor.rowcount == 1:
-			print "item added to purchases successfully"
-		db.commit()
-	# if the item already exists in purchases, update the quantity of that item
-	elif cursor.rowcount != 0:
-		cursor.execute("SELECT quantity FROM purchases WHERE barcode_num = '%s'" % barcode)
-		result = cursor.fetchall()
-		quantity = result[0][0]
-		quantity += 1
-		cursor.execute("UPDATE purchases SET quantity = '%s' WHERE barcode_num = '%s'" % (quantity, barcode))
-		if cursor.rowcount == 1:
-			print "item quantity updated successfully"
-		db.commit()
-	db.close()
+#		name = raw_input("Name: ")
+#		category = raw_input("Category: ")
+#	break out of the python script (for now. Come back and add this functionality later)
+		return
+#
+#
+#		# add item to database
+#		cursor.execute("INSERT INTO product_list(name, barcode_num, category) VALUES ('%s', '%s', '%s')" % (name, barcode, category))
+#		db.commit()
+	# add item to purchases list
+	cursor.execute("INSERT INTO purchases(barcode_num) VALUES ('%s')" % barcode)
+	db.commit()
+# if the item already exists in purchases, update the quantity of that item
+elif cursor.rowcount != 0:
+	cursor.execute("SELECT quantity FROM purchases WHERE barcode_num = '%s'" % barcode)
+	result = cursor.fetchall()
+	quantity = result[0][0]
+	quantity += 1
+	cursor.execute("UPDATE purchases SET quantity = '%s' WHERE barcode_num = '%s'" % (quantity, barcode))
+	db.commit()
+db.close()
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const engine = require('ejs-locals');
 const express = require('express');
 const bodyParser = require('body-parser');
+//const spawn = require('child_process').spawn;
 
 var actions = require('/var/www/node/controllers/actions');
 
@@ -11,25 +12,33 @@ var connectionsArray = [];
 var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
+var connection = require('express-myconnection');
+var mysql = require('mysql');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('ejs', engine); //use ejs as the templating engine
 app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/views'));
-
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/css'));
+app.use(express.statuc(__dirname + '/views'));
 
 server.listen(PORT, function(){
 	console.log('Server started on port ' + PORT)
 });
 
+app.use(connection(mysql,{
+	host: 'localhost',
+	user: 'emma',
+	password: 'sudosudo!!',
+	database: 'pantry'
+	}, 'pool')
+);
+
 app.get('/', function (req, res){
-        res.redirect('/smartfridge')
+	res.redirect('/smartfridge')
 });
 
-app.get('/smartfridge', function (req, res){
-	res.render('myHTML.ejs')
-});
+app.get('/smartfridge', actions.list);
 
 app.post('/addDB', function (req, res){
 	actions.addDB(req);
@@ -39,5 +48,10 @@ app.post('/addDB', function (req, res){
 app.post('/deleteDB', function (req, res){
         actions.deleteDB(req);
         res.redirect('/smartfridge#editdatabase');
+});
+
+app.post('/addPantry', function (req, res){
+        actions.addPantry(req);
+        res.redirect('/smartfridge#pantry');
 });
 
